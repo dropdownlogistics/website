ac441bca9d7ef5268204789a236da6d5bea5510c
DropDownLogistics <dropdownlogistics@gmail.com>
2026-03-14 01:37:48 -0500

    blindspot hub - remove competing page.tsx

--- app/blindspot/page.tsx
+++
@@ -1,6 +0,0 @@
-﻿import PreviewFrame from '@/components/PreviewFrame';
-
-export const metadata = { title: 'BlindSpot - D&A - DDL' };
-export default function Page() {
-  return <PreviewFrame src="/preview/blindspot-hub.html" title="BlindSpot - D&A" />;
-}
