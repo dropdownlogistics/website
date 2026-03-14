a4a800c1e69f9e05d36e3377544bc6f1f13fb6e8
DropDownLogistics <dropdownlogistics@gmail.com>
2026-03-14 01:15:09 -0500

    blindspot betting - blindspot.bet page, remove competing tsx

--- app/blindspot/betting/page.tsx
+++
@@ -1,6 +0,0 @@
-﻿import PreviewFrame from '@/components/PreviewFrame';
-
-export const metadata = { title: 'BlindSpot Betting - DDL' };
-export default function Page() {
-  return <PreviewFrame src="/preview/blindspot-betting.html" title="BlindSpot Betting" />;
-}
