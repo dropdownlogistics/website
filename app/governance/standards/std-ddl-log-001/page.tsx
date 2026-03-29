2ff922311bea16e8888f861373bf73b9fee91083
DropDownLogistics <dropdownlogistics@gmail.com>
2026-03-28 20:32:22 -0500

    feat: governance/standards wing — move pageprop-001, rec-001, add STD-DDL-LOG-001

---
+++ app/governance/standards/std-ddl-log-001/page.tsx
@@ -0,0 +1,3 @@
+﻿export default function StdDdlLog001() {
+  return <iframe src="/standards/std-ddl-log-001.html" style={{ position:'fixed', top:0, left:0, width:'100%', height:'100%', border:'none' }} />
+}
